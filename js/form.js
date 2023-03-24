224a6d4a3ab132cdb6a30cb5ddec7269755a57c4
ElenaAndryushihina <karetina1991@gmail.com>
2023-03-24 16:00:20 +0300

    Добавляет масштаб и фильтры

--- js/form.js
+++ js/form.js
@@ -1,3 +1,6 @@
+import {resetScale} from './scale.js';
+import {resetEffects} from './effect.js';
+
 const VALID_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
 const MAX_HASHTAG_COUNT = 5;
 const TAG_ERROR_TEXT = 'Хэштег введён некорректно';
@@ -25,6 +28,8 @@ const showModal = () => {
 
 const hideModal = () => {
   form.reset();
+  resetScale();
+  resetEffects();
   pristine.reset();
   overlay.classList.add('hidden');
   body.classList.remove('modal-open');
