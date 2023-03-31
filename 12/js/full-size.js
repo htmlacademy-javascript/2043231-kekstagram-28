ca7cf4e6fc4fcdf1fa8fb109b26f890177c4cce8
Keks <keks@htmlacademy.ru>
2023-03-31 23:20:47 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/full-size.js
@@ -0,0 +1,89 @@
+import {isEscapeKey} from './util.js';
+
+const COMMENTS_PER_PORTION = 5;
+
+const bigPicture = document.querySelector('.big-picture');
+const commentCount = document.querySelector('.social__comment-count');
+const commentList = document.querySelector('.social__comments');
+const commentItem = document.querySelector('.social__comment');
+const commentsLoader = document.querySelector('.comments-loader');
+const body = document.querySelector('body');
+const cancelButton = document.querySelector('.big-picture__cancel');
+
+let commentsShown = 0;
+let comments = [];
+
+const createComment = ({avatar, name, message}) => {
+  const comment = commentItem.cloneNode(true);
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
+const renderComments = () => {
+  commentsShown += COMMENTS_PER_PORTION;
+
+  if (commentsShown >= comments.length) {
+    commentsLoader.classList.add('hidden');
+    commentsShown = comments.length;
+  } else {
+    commentsLoader.classList.remove('hidden');
+  }
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < commentsShown; i++) {
+    const commentElement = createComment(comments[i]);
+    fragment.append(commentElement);
+  }
+
+  commentList.innerHTML = '';
+  commentList.append(fragment);
+  commentCount.innerHTML = `${commentsShown} из <span class="comments-count">${comments.length}</span> комментариев`;
+};
+
+const hideBigPicture = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onBigPictureKeydown);
+  commentsShown = 0;
+};
+
+function onBigPictureKeydown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    hideBigPicture();
+  }
+}
+
+const onCancelButtonClick = () => {
+  hideBigPicture();
+};
+
+const onCommentsLoaderClick = () => renderComments ();
+
+const renderPictureDetails = ({url, likes, description}) => {
+  bigPicture.querySelector('.big-picture__img img').src = url;
+  bigPicture.querySelector('.big-picture__img img').alt = description;
+  bigPicture.querySelector('.social__caption').textContent = description;
+  bigPicture.querySelector('.likes-count').textContent = likes;
+};
+
+const showFullSize = (data) => {
+  bigPicture.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onBigPictureKeydown);
+
+  renderPictureDetails(data);
+  comments = data.comments;
+  if (comments.length > 0) {
+    renderComments();
+  }
+};
+
+cancelButton.addEventListener('click', onCancelButtonClick);
+commentsLoader.addEventListener('click', onCommentsLoaderClick);
+
+export {showFullSize};
---
+++ 12/js/message.js
@@ -0,0 +1,76 @@
+import {isEscapeKey} from './util.js';
+import {onModalKeydown} from './form.js';
+
+const successMessage = document.querySelector('#success').content.querySelector('.success');
+const successButton = successMessage.querySelector('.success__button');
+const errorMessage = document.querySelector('#error').content.querySelector('.error');
+const errorButton = errorMessage.querySelector('.error__button');
+const body = document.querySelector('body');
+
+const onSuccessAnywhereClick = (evt) => {
+  if (evt.target === successMessage) {
+    document.querySelector('.success').remove();
+    document.removeEventListener('click', onSuccessAnywhereClick);
+    document.removeEventListener('keydown', onSuccessKeydown);
+  }
+};
+
+const onErrorAnywhereClick = (evt) => {
+  if (evt.target === errorMessage) {
+    document.querySelector('.error').remove();
+    document.removeEventListener('click', onErrorAnywhereClick);
+    document.removeEventListener('keydown', onErrorKeydown);
+  }
+};
+
+function onSuccessKeydown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    if (successMessage) {
+      document.querySelector('.success').remove();
+    }
+  }
+}
+
+function onErrorKeydown (evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    if (errorMessage) {
+      document.querySelector('.error').remove();
+      document.addEventListener('keydown', onModalKeydown);
+    }
+  }
+}
+
+const onSuccessButtonClick = () => {
+  if (successMessage) {
+    document.querySelector('.success').remove();
+  }
+};
+
+const onErrorButtonClick = () => {
+  if (errorMessage) {
+    document.querySelector('.error').remove();
+  }
+};
+
+const showSuccessMessage = () => {
+  successMessage.cloneNode(true);
+  body.append(successMessage);
+  document.addEventListener('keydown', onSuccessKeydown);
+  document.addEventListener('click', onSuccessAnywhereClick);
+};
+
+
+const showErrorMessage = () => {
+  errorMessage.cloneNode(true);
+  body.append(errorMessage);
+  document.removeEventListener('keydown', onModalKeydown);
+  document.addEventListener('keydown', onErrorKeydown);
+  document.addEventListener('click', onErrorAnywhereClick);
+};
+
+successButton.addEventListener('click', onSuccessButtonClick);
+errorButton.addEventListener('click', onErrorButtonClick);
+
+export{showSuccessMessage, showErrorMessage};
